a9a258dd495c82cc340627c8b341aeda7d433072
Aiirinz <airin1311som@gmail.com>
2026-09-23 17:42:13 +0700

    Add cell06/ex03 greetings_for_all.py

---
+++ cell06/ex03/greetings_for_all.py
@@ -0,0 +1,11 @@
+def greetings(name="noble stranger"):
+  if type(name) != str:
+    print("Error! It was not a name.")
+  else:
+    print("Hello, " + name + ".")
+
+
+greetings("Alexandra")
+greetings("Wil")
+greetings()
+greetings(42)
